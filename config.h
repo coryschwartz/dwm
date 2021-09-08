341560ec429a847c162c38d25953aab53ec7ad80
Cory Schwartz <cory@protocol.ai>
2021-09-08 12:52:37 -0700

    sakura isterminal

--- config.h
+++ config.h
@@ -9,11 +9,11 @@ static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corne
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const unsigned int gappih    = 15;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 15;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -44,7 +44,7 @@ static const Rule rules[] = {
 	/* { "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 }, */
 	/* { "St",      NULL,     NULL,           0,         0,          1,           0,        -1 }, */
 	/* { NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /1* xev *1/ */
-	{}
+	{ "Sakura",     NULL,     NULL,           0,         0,          1,           0,        -1 },
 };
 
 /* layout(s) */
